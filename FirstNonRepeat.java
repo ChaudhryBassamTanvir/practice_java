fb1b078658a06f5ecfbeca8ac91a3034f780542b
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-12-20 13:44:56 +0500

    Updated

---
+++ FirstNonRepeat.java
@@ -0,0 +1,30 @@
+public class FirstNonRepeat {
+    public static void main(String[] args) {
+     //Find the first non-repeating character
+        String word="Bassam";
+
+for(int i=0;i<word.length()-1;i++){
+  boolean repeated= false;
+for (int j = 0; j < word.length(); j++) {
+ if (i!=j && word.charAt(i)==word.charAt(j)) {
+    repeated=true;
+    break;
+    
+ }  
+
+}
+if (!repeated) {
+  System.out.println(word.charAt(i));  
+    
+ }  
+}
+
+//Capitalize the first letter of each word.
+String sentence="Java is the best programming";
+String[] words = sentence.split("\\s+");
+
+
+
+
+    }
+}
--- FrequencyOfChar.java
+++ FrequencyOfChar.java
@@ -11,6 +11,23 @@ public class FrequencyOfChar {
             }
         }
 System.out.println("Character Frequency " + charCountMap);
-        
+      
+
+// Count words in a sentence
+String sentence = "This is java programming";
+String[] words= sentence.split("\\s+");
+System.out.println("the number of words in a sentence is "+words.length);
+
+// Special sequence charater exist or not
+String word="Bassam";
+String existWord="Bas";
+char existChar='s';
+
+System.out.println(word.contains(existWord));
+System.out.println(word.contains(String.valueOf(existChar)));
+
+
+
+
     }
 }
