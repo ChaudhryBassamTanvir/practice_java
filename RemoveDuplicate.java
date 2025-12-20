4570828ed384282963f44a9396c914ae66ef1600
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-12-20 14:15:40 +0500

    Updated

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -5,13 +5,19 @@ public class RemoveDuplicate {
         String newStr = "";
 
     for(int i=0;i<str.length();i++){
+        boolean repeated=false;
         for (int j = 0; j < str.length(); j++) {
         if (i!=j && str.charAt(i)==str.charAt(j)) {
+          repeated=true;
             break;            
         }            
         }
-        newStr+=str.charAt(i);
+        if (!repeated) {
+            
+            newStr+=str.charAt(i);
+        }
     }
-System.out.println(newStr);
+    System.out.println(newStr);
+
     }
 }
