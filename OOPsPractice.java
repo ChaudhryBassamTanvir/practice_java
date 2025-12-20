06ecc6f8d09642436b3e1b89d533544d6aed6a4b
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-12-20 23:42:42 +0500

    Updated

---
+++ OOPsPractice.java
@@ -0,0 +1,22 @@
+public class OOPsPractice {
+  
+    public static void main(String[] args) {
+          class Student{
+     String name;
+        int rollno;
+
+         Student(String name,int rollno) {
+        this.name=name;
+        this.rollno=rollno;
+        }
+
+        
+
+
+    }
+Student st1 = new Student("Bani", 1);
+System.out.println(st1.name+" "+st1.rollno);
+        
+
+    }
+}
