5c88a7a12ab672e39ff76cb6348f7ccf2cd9e0a1
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2026-01-04 12:52:24 +0500

    Updated

--- SortMapByValues.java
+++ SortMapByValues.java
@@ -1,5 +1,20 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
 public class SortMapByValues {
     public static void main(String[] args) {
+        HashMap<String,Integer>hashmap=new HashMap<>();
+        hashmap.put("Ali", 92);
+        hashmap.put("Bassam", 99);
+        hashmap.put("umer", 89);
+
+      Map<String, Integer> sortedMap= hashmap.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(
+        Map.Entry::getKey,
+        Map.Entry::getValue,
+        (e1,e2)->e1,
+        LinkedHashMap::new
         
+        ));
+        System.out.println(sortedMap);
     }
 }
